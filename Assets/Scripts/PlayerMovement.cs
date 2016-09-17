1ce8ef8581d2bb17ca872232d96056af72082336
crabcrabcam <cam12win@gmail.com>
2016-09-17 18:30:44 +0100

    Playermovemet

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float jumpHeight;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        if (Input.GetAxisRaw("Horizontal") > 0.1)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.forward * speed);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") < -0.1)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.forward * -speed);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.1)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+    }
+
+}
